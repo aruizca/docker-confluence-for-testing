974c50f039b0ea43474d5d29ba91994677331843
Jalonso063 <jalonso063@gmail.com>
2022-10-26 17:16:11 +0200

    Fixes puppeteer container name in each shell script

--- scripts/run-confluence-container-no-logs.sh
+++ scripts/run-confluence-container-no-logs.sh
@@ -3,6 +3,6 @@
 source run-confluence-container-common.sh $@
 
 docker-compose -p ${PACKAGE_NAME} up -d ${DATABASE} puppeteer-confluence-setup
-docker logs -f puppeteer-confluence-setup
+docker logs -f puppeteer-confluence-setup_${PACKAGE_NAME}
 
 
--- scripts/run-confluence-container.sh
+++ scripts/run-confluence-container.sh
@@ -3,5 +3,5 @@
 source run-confluence-container-common.sh $@
 
 docker-compose -p ${PACKAGE_NAME} up -d ${DATABASE} puppeteer-confluence-setup
-docker logs -f puppeteer-confluence-setup
+docker logs -f puppeteer-confluence-setup_${PACKAGE_NAME}
 docker logs -f confluence_${PACKAGE_NAME}
