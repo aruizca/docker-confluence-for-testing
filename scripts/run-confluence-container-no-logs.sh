09902fe133ad4b23cd6d136fa148eae068808886
Pablo Gallego <pablo@comalatech.com>
2022-10-27 10:18:18 +0200

    Fixed relative path to invoke common

--- scripts/run-confluence-container-no-logs.sh
+++ scripts/run-confluence-container-no-logs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source run-confluence-container-common.sh $@
+source ${BASH_SOURCE%/*}/run-confluence-container-common.sh $@
 
 docker-compose -p ${PACKAGE_NAME} up -d ${DATABASE} puppeteer-confluence-setup
 docker logs -f puppeteer-confluence-setup_${PACKAGE_NAME}
--- scripts/run-confluence-container-no-setup.sh
+++ scripts/run-confluence-container-no-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source run-confluence-container-common.sh $@
+source ${BASH_SOURCE%/*}/run-confluence-container-common.sh $@
 
 docker-compose -p ${PACKAGE_NAME} up -d ${DATABASE} confluence
 docker logs -f confluence_${PACKAGE_NAME}
